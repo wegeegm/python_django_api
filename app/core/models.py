dc40116b9bc3f7f721142375a33fa9e0a1204c6d
wegeegm <michael.eggenschwiler.me@gmail.com>
2020-08-26 19:57:15 +0200

    update to the usermodel

--- app/core/models.py
+++ app/core/models.py
@@ -32,7 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_statt = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
